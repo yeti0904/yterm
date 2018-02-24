6e2fccf1dbf7af9b6492855b67ac9e2eaaa3d912
Peter Hofmann <scm@uninformativ.de>
2018-02-24 16:08:10 +0100

    Improve scrolling
    (This is where things start to get nasty.)

--- eduterm.c
+++ eduterm.c
@@ -308,6 +308,7 @@ run(struct PTY *pty, struct X11 *x11)
     fd_set readable;
     XEvent ev;
     char buf[1];
+    bool just_wrapped = false;
 
     maxfd = pty->master > x11->fd ? pty->master : x11->fd;
 
@@ -334,34 +335,66 @@ run(struct PTY *pty, struct X11 *x11)
                 return 1;
             }
 
-            /* Save the byte we just read into the next free cell and
-             * then advance to "the next cell". We'll soon figure out
-             * what that means. */
-            x11->buf[x11->buf_y * x11->buf_w + x11->buf_x] = buf[0];
-            x11->buf_x++;
-
-            /* If we read a newline or if we reached the right end of
-             * the window, we're going to advance to "the next line". */
-            if (buf[0] == '\n' || x11->buf_x >= x11->buf_w)
+            if (buf[0] == '\r')
             {
+                /* "Carriage returns" are probably the most simple
+                 * "terminal command": They just make the cursor jump
+                 * back to the very first column. */
                 x11->buf_x = 0;
-                x11->buf_y++;
             }
-
-            /* We now check if "the next line" is actually outside of
-             * the buffer. If it is, we shift the entire content one
-             * line up and then stay in the very last line.
-             *
-             * After the memmove(), the last line still has the old
-             * content. We must clear it. */
-            if (x11->buf_y >= x11->buf_h)
+            else
             {
-                memmove(x11->buf, &x11->buf[x11->buf_w],
-                        x11->buf_w * (x11->buf_h - 1));
-                x11->buf_y = x11->buf_h - 1;
+                if (buf[0] != '\n')
+                {
+                    /* If this is a regular byte, store it and advance
+                     * the cursor one cell "to the right". This might
+                     * actually wrap to the next line, see below. */
+                    x11->buf[x11->buf_y * x11->buf_w + x11->buf_x] = buf[0];
+                    x11->buf_x++;
 
-                for (i = 0; i < x11->buf_w; i++)
-                    x11->buf[x11->buf_y * x11->buf_w + i] = 0;
+                    if (x11->buf_x >= x11->buf_w)
+                    {
+                        x11->buf_x = 0;
+                        x11->buf_y++;
+                        just_wrapped = true;
+                    }
+                    else
+                        just_wrapped = false;
+                }
+                else if (!just_wrapped)
+                {
+                    /* We read a newline and we did *not* implicitly
+                     * wrap to the next line with the last byte we read.
+                     * This means we must *now* advance to the next
+                     * line.
+                     *
+                     * This is the same behaviour that most other
+                     * terminals have: If you print a full line and then
+                     * a newline, they "ignore" that newline. (Just
+                     * think about it: A full line of text could always
+                     * wrap to the next line implicitly, so that
+                     * additional newline could cause the cursor to jump
+                     * to the next line *again*.) */
+                    x11->buf_x = 0;
+                    x11->buf_y++;
+                    just_wrapped = false;
+                }
+
+                /* We now check if "the next line" is actually outside
+                 * of the buffer. If it is, we shift the entire content
+                 * one line up and then stay in the very last line.
+                 *
+                 * After the memmove(), the last line still has the old
+                 * content. We must clear it. */
+                if (x11->buf_y >= x11->buf_h)
+                {
+                    memmove(x11->buf, &x11->buf[x11->buf_w],
+                            x11->buf_w * (x11->buf_h - 1));
+                    x11->buf_y = x11->buf_h - 1;
+
+                    for (i = 0; i < x11->buf_w; i++)
+                        x11->buf[x11->buf_y * x11->buf_w + i] = 0;
+                }
             }
 
             x11_redraw(x11);
