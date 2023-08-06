b3df435e18d8090f97bc36b0a0d6ebc37e02e5a1
LordOfTrident <lordoftrident@gmail.com>
2023-08-06 19:57:02 +0200

    Fix this total mess

--- source/escapes.c
+++ source/escapes.c
@@ -35,51 +35,23 @@ static bool NextByte(Terminal* terminal, char* byte) {
 }
 
 static uint8_t FGColour(int colour) {
-	switch (colour) {
-		case 30: return COLOUR_BLACK;
-		case 31: return COLOUR_RED;
-		case 32: return COLOUR_GREEN;
-		case 33: return COLOUR_YELLOW;
-		case 34: return COLOUR_BLUE;
-		case 35: return COLOUR_MAGENTA;
-		case 36: return COLOUR_CYAN;
-		case 37: return COLOUR_WHITE;
-		case 90: return COLOUR_GREY;
-		case 91: return COLOUR_BRIGHT_RED;
-		case 92: return COLOUR_BRIGHT_GREEN;
-		case 93: return COLOUR_BRIGHT_YELLOW;
-		case 94: return COLOUR_BRIGHT_BLUE;
-		case 95: return COLOUR_BRIGHT_MAGENTA;
-		case 96: return COLOUR_BRIGHT_CYAN;
-		case 97: return COLOUR_BRIGHT_WHITE;
+	if (colour >= 30 && colour <= 37) {
+		return colour - 30;
 	}
-
-	FATAL("Invalid colour");
-	return 0;
+	else if (colour >= 90 && colour <= 97) {
+		return colour - 90 + COLOUR_GREY;
+	}
+	else {
+		return (uint8_t) -1;
+	}
 }
 
 static uint8_t BGColour(int colour) {
-	switch (colour) {
-		case 40:  return COLOUR_BLACK;
-		case 41:  return COLOUR_RED;
-		case 42:  return COLOUR_GREEN;
-		case 43:  return COLOUR_YELLOW;
-		case 44:  return COLOUR_BLUE;
-		case 45:  return COLOUR_MAGENTA;
-		case 46:  return COLOUR_CYAN;
-		case 47:  return COLOUR_WHITE;
-		case 100: return COLOUR_GREY;
-		case 101: return COLOUR_BRIGHT_RED;
-		case 102: return COLOUR_BRIGHT_GREEN;
-		case 103: return COLOUR_BRIGHT_YELLOW;
-		case 104: return COLOUR_BRIGHT_BLUE;
-		case 105: return COLOUR_BRIGHT_MAGENTA;
-		case 106: return COLOUR_BRIGHT_CYAN;
-		case 107: return COLOUR_BRIGHT_WHITE;
+	if (colour < 40) {
+		return (uint8_t) -1;
 	}
 
-	FATAL("Invalid colour");
-	return 0;
+	return FGColour(colour - 10);
 }
 
 static void RunCommand(Terminal* terminal, char cmd, int* args, size_t argsCount) {
@@ -111,7 +83,7 @@ static void RunCommand(Terminal* terminal, char cmd, int* args, size_t argsCount
 			if (terminal->buffer.cursor.y >= terminal->buffer.size.y) {
 				int difference =
 					terminal->buffer.cursor.y - terminal->buffer.size.y;
-					
+
 				TextScreen_ScrollDown(&terminal->buffer, difference + 1);
 			}
 			break;
@@ -135,7 +107,7 @@ static void RunCommand(Terminal* terminal, char cmd, int* args, size_t argsCount
 		// erase commands
 		case 'J': {
 			Cell emptyCell = CellByCharacter(' ');
-			
+
 			if ((argsCount == 0) || (args[0] == 0)) { // until end of line
 				for (
 					int i = terminal->buffer.cursor.x;
@@ -235,45 +207,16 @@ static void RunCommand(Terminal* terminal, char cmd, int* args, size_t argsCount
 						terminal->buffer.attr.attr &= ~ATTR_COLOUR_BG;
 						break;
 					}
-					case 30:
-					case 31:
-					case 32:
-					case 33:
-					case 34:
-					case 35:
-					case 36:
-					case 37:
-					case 90:
-					case 91:
-					case 92:
-					case 93:
-					case 94:
-					case 95:
-					case 96:
-					case 97: {
-						terminal->buffer.attr.attr |= ATTR_COLOUR_FG;
-						terminal->buffer.attr.fg    = FGColour(args[i]);
-						break;
-					}
-					case 40:
-					case 41:
-					case 42:
-					case 43:
-					case 44:
-					case 45:
-					case 46:
-					case 47:
-					case 100:
-					case 101:
-					case 102:
-					case 103:
-					case 104:
-					case 105:
-					case 106:
-					case 107: {
-						terminal->buffer.attr.attr |= ATTR_COLOUR_BG;
-						terminal->buffer.attr.bg    = BGColour(args[i]);
-						break;
+					default: {
+						uint8_t colour;
+						if ((colour = FGColour(args[i])) != (uint8_t) -1) {
+							terminal->buffer.attr.attr |= ATTR_COLOUR_FG;
+							terminal->buffer.attr.fg    = colour;
+						}
+						else if ((colour = BGColour(args[i])) != (uint8_t) -1) {
+							terminal->buffer.attr.attr |= ATTR_COLOUR_FG;
+							terminal->buffer.attr.bg    = colour;
+						}
 					}
 				}
 			}
@@ -292,7 +235,7 @@ static void RunCommand(Terminal* terminal, char cmd, int* args, size_t argsCount
 
 void InterpretOption(Terminal* terminal, int option, char value) {
 	(void) terminal;
-	
+
 	switch (option) {
 		default: {
 			printf("Set option %d to %c", option, value);
@@ -344,7 +287,7 @@ void HandleEscape(Terminal* terminal) {
 		// TODO: maybe i should make this not a static array
 		int    args[256];
 		size_t argsCount = 0;
-		
+
 		readStr    = SafeMalloc(1);
 		readStr[0] = 0;
 
